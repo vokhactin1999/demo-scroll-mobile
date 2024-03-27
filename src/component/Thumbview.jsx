5e92ab345ffea50aaadb175ab8a3d9d685585331
Tin Vo <vokhactin1234567@gmail.com>
2024-03-27 23:43:56 +0700

    change style

--- src/component/Thumbview.jsx
+++ src/component/Thumbview.jsx
@@ -14,7 +14,7 @@ const Thumbview = ({pickerValue}) => {
     }
     return (
         <div style={{height:'440px',width:'100%', display:'flex',alignItems:'center',justifyContent:'center'}}>
-            <div style={{width:'30%'}}>
+            <div style={{width:'40%'}}>
             {renderImageTag()}
             </div>
      
