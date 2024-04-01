f7340d29e2fa048f361419013f8763a609d7cf45
Tin Vo <vokhactin1234567@gmail.com>
2024-04-02 01:56:32 +0700

    update code

--- src/component/GagoScrollingMobile.jsx
+++ src/component/GagoScrollingMobile.jsx
@@ -18,7 +18,7 @@ function GagoScrollingMobile() {
 				onChange={getSelectedElementId}
 				wheel='normal'
 				height={200}
-				itemHeight={36}
+				itemHeight={42}
 				className='picker-scroll'>
 				<Picker.Column key={name} name='gagoId'>
 					{data_temoin.map((option) => (
